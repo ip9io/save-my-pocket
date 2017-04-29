6a06eda8dc5510fbc8b688b30ec11ced831eefe2
ip9io <labs@ip9.io>
2017-04-29 23:07:55 +0200

    add config and settings system

--- bootstrap.rb
+++ bootstrap.rb
@@ -3,4 +3,7 @@ require 'bundler'
 Bundler.require
 
 ROOT_PATH = File.expand_path File.dirname(__FILE__)
+CONF_PATH = File.join ROOT_PATH, 'config'
+
+require File.join CONF_PATH, 'settings'
 
---
+++ config/SAMPLE.settings.rb
@@ -0,0 +1,5 @@
+
+APP_URL = 'http://localhost:4567'
+
+POCKET_CONSUMER_KEY = '-your-key-'
+
