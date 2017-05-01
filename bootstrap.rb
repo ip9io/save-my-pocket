b90dc169be081da8e9050ad66059566d3d136e70
ip9io <labs@ip9.io>
2017-05-01 03:05:47 +0200

    add libraries loading to bootstrap

--- bootstrap.rb
+++ bootstrap.rb
@@ -4,6 +4,10 @@ Bundler.require
 
 ROOT_PATH = File.expand_path File.dirname(__FILE__)
 CONF_PATH = File.join ROOT_PATH, 'config'
+LIBS_PATH = File.join ROOT_PATH, 'libs'
 
 require File.join CONF_PATH, 'settings'
+
+# Load libraries
+Dir.glob(File.join LIBS_PATH, '*.rb').each { |f| require f }
 
