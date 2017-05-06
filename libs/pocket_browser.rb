833f31d8a9766ef036dca5103b1a745a331689a0
ip9io <labs@ip9.io>
2017-05-06 23:58:39 +0200

    refactor pocket browser

--- libs/pocket_browser.rb
+++ libs/pocket_browser.rb
@@ -3,17 +3,15 @@
 #
 class PocketBrowser
 
-  @@browser = nil
-
-  def self.get
-    return @@browser unless @@browser.nil?
-    @@browser = Watir::Browser.new :phantomjs
-    @@browser.goto APP_URL + '/pocket/oauth/connect'
-    @@browser.text_field(name: 'feed_id').set POCKET_USER
-    @@browser.text_field(name: 'password').set POCKET_PWD
-    @@browser.input(value: 'Authorize').click
+  def self.get_authorized(type=nil)
+    type = :phantomjs if type.nil?
+    browser = Watir::Browser.new type
+    browser.goto APP_URL + '/pocket/oauth/connect'
+    browser.text_field(name: 'feed_id').set POCKET_USER
+    browser.text_field(name: 'password').set POCKET_PWD
+    browser.input(value: 'Authorize').click
     sleep 2
-    @@browser
+    browser
   end
 
 end
