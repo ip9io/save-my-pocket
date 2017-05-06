ea9f0d2b507b6bb5a4bc13f2f67b838c277191e5
ip9io <labs@ip9.io>
2017-05-07 01:24:14 +0200

    update cron to send an email when an exception occur

--- bin/cron.rb
+++ bin/cron.rb
@@ -6,7 +6,17 @@ browser = PocketBrowser.get_authorized
 bookmark_manager = BookmarkManager.new browser
 
 
-every 1.day, 'sync bookmarks', at: '00:00', thread: true do
-  bookmark_manager.sync
+# every 1.day, 'sync bookmarks', at: '00:00', thread: true do
+
+every 1.minute, 'sync bookmarks', thread: true do
+  begin
+    bookmark_manager.sync
+  rescue Exception => e
+    msg = "Message : #{e.message}\n\n"
+    msg << "Backtrace :\n\n"
+    msg << e.backtrace.join("\n")
+
+    MailHelper.send '[ALERT] : Error with save my pocket', msg
+  end
 end
 
