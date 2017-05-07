2c744c6f6f37d585556fe237cdb3c680fb6f1887
ip9io <labs@ip9.io>
2017-05-07 19:18:30 +0200

    add report format tmp test

---
+++ tmp/report_format.rb
@@ -0,0 +1,55 @@
+report = [
+  {
+    :id=>"219508577",
+    :action=>"created",
+    :tags=>["+jod", "+scheduler", "+sidekiq"],
+    :title=>"Simple, efficient job processing for Ruby"
+  },
+  {
+    :id=>"1685314650",
+    :action=>"updated",
+    :tags=>["-code", "-phoenix", "+master", "+test"],
+    :title=>"How we built passwordless authentication with Auth0 and Elixir/Phoenix"
+  },
+  {
+    :id=>"1179255400",
+    :action=>"deleted",
+    :title=>"Buttercup"
+  }
+]
+
+
+class ReportHelper
+
+  def self.format(report)
+    result = "[#{Time.now}]\n"
+
+    if report.size == 0
+      result << "-- nothing have been done --\n"
+    end
+
+    report.each do |log|
+      result << "#{log[:action]} : #{log[:id]} : #{log[:title]}\n"
+      if log.key? :tags
+        tags = log[:tags]
+        unless tags.size == 0
+          result << "   #{tags.join ' '}\n"
+        end
+      end
+    end
+
+    result
+  end
+
+end
+
+
+puts ReportHelper.format(report)
+
+
+
+
+
+
+
+
